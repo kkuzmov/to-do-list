b242c16f6460d461fabd747560cda8dadcb593dc
Krum Kuzmov <kkuzmov@gmail.com>
2021-06-11 22:58:43 +0300

    add edit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,11 +15,21 @@ function TodoList() {
     console.log(todos)
   };
 
+  const updateTodo = (todoId, newValue) =>{
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item =>(item.id === todoId ? newValue : item)))
+  }
+
+
   const removeTodo = id =>{
     const removeArr = [...todos].filter(todo => todo.id !== id);
 
     setTodos(removeArr)
   }
+
 
   const completeTodo = id =>{
     let updatedTodos = todos.map(todo =>{
@@ -40,6 +50,7 @@ function TodoList() {
       todos={todos}
       completeTodo={completeTodo}
       removeTodo={removeTodo}
+      updateTodo={updateTodo}
       />
     </div>
   );
