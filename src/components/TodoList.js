342c474c76c211e85b9c2bc8673f7deb9f15ab2b
Krum Kuzmov <kkuzmov@gmail.com>
2021-06-11 21:30:46 +0300

    fix todo and todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,41 @@
-import React, { useState } from 'react'
-import TodoForm from './TodoForm'
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoList() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-  const addTodo = todo =>{
-    if(!todo.text || /^\s*$/.test(todo.text)){
-      return
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
-    const newTodos = [todo, ...todos]
+    const newTodos = [todo, ...todos];
 
-    setTodos(newTodos)
+    setTodos(newTodos);
+    console.log(todos)
+  };
+
+  const completeTodo = id =>{
+    let updatedTodos = todos.map(todo =>{
+      if(todo.id === id){
+        todo.isComplete = !todo.isComplete
+      }
+      return todo
+    })
+    
+    setTodos(updatedTodos)
   }
-  
+
   return (
     <div>
       <h1>What's the plan for today?</h1>
-      <TodoForm onSubmit={addTodo}/>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+      todos={todos}
+      completeTodo={completeTodo}
+      />
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
